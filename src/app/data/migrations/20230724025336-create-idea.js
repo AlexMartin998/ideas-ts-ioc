3abc1224bc2b56d07921501dccf979885e83fdf6
Adrian <alextravc.342@gmail.com>
2023-07-23 21:59:32 -0500

    [ADD]: idea migration

---
+++ src/app/data/migrations/20230724025336-create-idea.js
@@ -0,0 +1,42 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('ideas', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      description: {
+        type: Sequelize.STRING,
+      },
+      user_id: {
+        type: Sequelize.DataTypes.INTEGER,
+        references: {
+          model: {
+            tableName: 'users',
+            // schema: 'schema',
+          },
+          key: 'id',
+        },
+        allowNull: false,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('ideas');
+  },
+};
--- src/app/data/models/index.ts
+++ src/app/data/models/index.ts
@@ -1,5 +1,8 @@
 import User from './user.model';
+import Idea from './idea.model';
 
 // associations
+User.hasMany(Idea, { foreignKey: 'user_id' });
+Idea.belongsTo(User);
 
-export { User };
+export { User, Idea };
