fc4d7a54d431d4587f1753a2f2bb8f877b69f7fd
Adrian <alextravc.342@gmail.com>
2023-07-23 20:03:07 -0500

    [UPD]: chalk dependency version for logger

--- src/app/services/interfaces/dto/LoginDto.ts
+++
@@ -1,6 +0,0 @@
-import { Model } from 'sequelize';
-
-export interface LoginDto extends Model<LoginDto> {
-  email: string;
-  password: string;
-}
--- src/app/services/interfaces/dto/RegisterDto.ts
+++
@@ -1,9 +0,0 @@
-import { Model } from 'sequelize';
-
-export interface RegisterDto extends Model<RegisterDto> {
-  first_name: string;
-  last_name: string;
-  email: string;
-  username: string;
-  password: string;
-}
